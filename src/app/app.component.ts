18a5c3b37791165733d5e82cc24f359d4fd3efae
vhoangssf <vincenthoang00@gmail.com>
2017-09-19 05:51:21 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AgeValidator } from '../validators/age';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  myForm: FormGroup;
+  submitAttempt: boolean = false;
+  
+  constructor(public formBuilder: FormBuilder) {
+    this.myForm = formBuilder.group({
+        userName: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        age: ['', AgeValidator.isValid],
+        email: ['',
+          Validators.compose([
+            Validators.required, 
+            Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$')]
+          )],
+        password: ['', Validators.required]
+    });
+  }
+  
+  submit() {
+    this.submitAttempt = true;
+    if(!this.myForm.valid) {
+      console.log("Unsuccessful registration :(");
+    } else {
+      alert('Thank you for registering!');
+      console.log("Successful registration!", this.myForm.value);
+    }
+  }
+  
 }
